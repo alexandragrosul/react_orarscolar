db7273b5c705b4dc324b1070f8ef8351f4b1c087
Grosul Alexandra <grosulalexandra@gmail.com>
2023-06-15 16:55:07 +0300

    Co-authored-by: Alexander-Uzuz <Alexander-Uzuz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Timetable from "./pages/Timetable";
 import Wrapper from "./components/Wrapper";
 import Schools from "./pages/Schools";
 import RepetitoriPage from "./pages/RepetitoriPage";
+import SchoolPage from "./pages/SchoolPage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Wrapper />}>
           <Route path="/" element={<Home />} />
+          <Route path="/repetitori" element={<RepetitoriPage />} />
           <Route path="/repetitori/:id" element={<Repetitor />} />
           <Route path="/timetable/:schoolId" element={<Timetable />} />
           <Route path="/schools" element={<Schools />} />
-          <Route path="/repetitori" element={<RepetitoriPage />} />
+          <Route path="/schools/:id" element={<SchoolPage />} />
         </Route>
       </Routes>
     </>
--- src/components/School.jsx
+++ src/components/School.jsx
@@ -11,93 +11,27 @@ const School = ({ school }) => {
   const greyLight = grey[400];
 
   return (
-    <Link to={`/repetitori/${school.id}`}>
-      <Card
-        sx={{ borderRadius: 10, border: "solid 1px green", marginBottom: 15 }}
-        key={school.id}
-      >
+    <Link to={`/schools/${school.id}`}>
+      <Card sx={{ marginBottom: 3 }} key={school.id}>
         <CardContent>
           <Grid container sx={{ alignItems: "center" }}>
-            <Grid
-              item
-              xs={12}
-              sm={10}
-              sx={{
-                order: {
-                  xs: 2,
-                  sm: 1,
-                },
-              }}
-            >
-              <Box
-                sx={{
-                  textAlign: {
-                    xs: "center",
-                    sm: "left",
-                  },
-                }}
-              >
-                <Typography sx={{ fontWeight: 700 }} variant="h6">
-                  {school.name}
-                  {/* {profesor.material.map((material) => (
-                    <Chip
-                      color="success"
-                      label={material}
-                      key={material}
-                      sx={{ marginTop: "8px !important" }}
-                    />
-                  ))} */}
-                </Typography>
-                <ul>
-                  {school.serviceAdress.map((adress) => (
-                    <li>{adress}</li>
-                  ))}
-                </ul>
-                <Box>
-                  <Typography>Phone:</Typography>
-                  <Typography>{school.phone}</Typography>
-                </Box>
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{
-                    justifyContent: {
-                      xs: "center",
-                      sm: "flex-start",
-                    },
-                    flexWrap: "wrap",
-                  }}
-                >
-                  {/* {profesor.languages.map((language) => (
-                    <Chip
-                      label={language}
-                      key={language}
-                      sx={{
-                        marginTop: "8px !important",
-                        backgroundColor: greyLight,
-                      }}
-                    />
-                  ))} */}
-                </Stack>
-              </Box>
+            <Grid item xs={12} md={4}>
+              <img src="https://picsum.photos/300/200" />
             </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography sx={{ fontWeight: 700 }} variant="h6">
+                {school.name}
+              </Typography>
 
-            <Grid
-              item
-              xs={12}
-              sm={2}
-              sx={{
-                order: {
-                  xs: 1,
-                  sm: 2,
-                },
-                display: "flex",
-                justifyContent: {
-                  xs: "center",
-                  sm: "flex-start",
-                },
-              }}
-            >
+              <Box>
+                <Typography>Phone:</Typography>
+                <Typography>{school.phone}</Typography>
+              </Box>
+              <ul>
+                {school.serviceAdress.map((adress, index) => (
+                  <li key={index}>{adress}</li>
+                ))}
+              </ul>
               <Typography>{school.schoolAdress}</Typography>
             </Grid>
           </Grid>
