325e0696cd87042f6f96d50e4075539e09a74a0c
Alexander <uzuz98@mail.ru>
2023-10-09 11:57:48 +0300

    add useCallback in dependencies

--- src/pages/SchoolPage.jsx
+++ src/pages/SchoolPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import {
   Card,
   CardContent,
