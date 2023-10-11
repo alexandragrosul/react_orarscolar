5a49508e0f605ae299897db6e21b2a93f3605960
Alexander <uzuz98@mail.ru>
2023-10-11 16:56:36 +0300

    add lessons

--- src/components/timetable/Timetable.jsx
+++ src/components/timetable/Timetable.jsx
@@ -12,42 +12,60 @@ import {
 import FolderIcon from "@mui/icons-material/Folder";
 import DeleteIcon from "@mui/icons-material/Delete";
 
+const colors = [
+  "red",
+  "purple",
+  "yellow",
+  "green",
+  "blue",
+  "orange",
+  "pink",
+  "brown",
+  "teal",
+  "gray",
+];
+const schedule = ["Математика", "Русский", "Английский"];
+
+function getRandomColor() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
 export const TimeTable = () => {
   const [dense] = React.useState(false);
   const [secondary] = React.useState(false);
 
-  function generate(element) {
-    return [0, 1, 2].map((value) =>
-      React.cloneElement(element, {
-        key: value,
-      })
-    );
-  }
   return (
     <Grid item xs={12} md={6}>
       <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
         Avatar with text and icon
       </Typography>
       <List dense={dense}>
-        {generate(
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            }
-          >
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="Single-line item"
-              secondary={secondary ? "Secondary text" : null}
-            />
-          </ListItem>
-        )}
+        {schedule.map((item) => {
+          return (
+            <ListItem
+              key={item}
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              }
+              sx={{ 
+                backgroundColor:getRandomColor(),
+               }}
+            >
+              <ListItemAvatar>
+                <Avatar>
+                  <FolderIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={item + ' ' + '8:30 - 12:00'}
+                secondary={secondary ? "Secondary text" : null}
+              />
+            </ListItem>
+          );
+        })}
       </List>
     </Grid>
   );
